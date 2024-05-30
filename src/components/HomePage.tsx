2b6b1dcce4ef816ad628e2c2925381aaa8f084b1
donikamarkowa <donimarkova@gmail.com>
2024-05-30 13:19:17 +0300

    Add additional navigation buttons to HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,9 +5,14 @@ const HomePage: React.FC = () => {
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
-            {/* Add content for the home page */}
             <Link to="/workouts">
                 <button>Go to Workouts</button>
+            </Link>
+            <Link to="/trainers">
+                <button>Go to Trainers</button>
+            </Link>
+            <Link to="/profile/edit">
+                <button>Edit profile info</button>
             </Link>
         </div>
     );
