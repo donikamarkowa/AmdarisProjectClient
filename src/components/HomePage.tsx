cc6af1d16cf3cbf054bb6b78465958a5981d9589
donikamarkowa <donimarkova@gmail.com>
2024-05-30 11:32:50 +0300

    Add navigation button to workouts page on HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage: React.FC = () => {
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
             {/* Add content for the home page */}
+            <Link to="/workouts">
+                <button>Go to Workouts</button>
+            </Link>
         </div>
     );
 };
-
 
 export default HomePage;
