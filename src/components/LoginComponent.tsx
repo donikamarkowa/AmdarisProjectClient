aa3e07dca59967891dc136f74963339f9d3e863f
donikamarkowa <donimarkova@gmail.com>
2024-06-04 11:55:09 +0300

    Remove comments

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -68,26 +68,6 @@ const Login: React.FC = () => {
         </Container>
       );
     };
-    
-//     return (
-//         <div>
-//             <h2>Login</h2>
-//             <input
-//                 type="email"
-//                 placeholder="Email"
-//                 value={email}
-//                 onChange={e => setEmail(e.target.value)}
-//             />
-//             <input
-//                 type="password"
-//                 placeholder="Password"
-//                 value={password}
-//                 onChange={e => setPassword(e.target.value)}
-//             />
-//             <button onClick={handleLogin}>Login</button>
-//         </div>
-//     );
-// };
 
 export default Login;
 
