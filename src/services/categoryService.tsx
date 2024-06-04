f45a5fee72eeb33ca2e898d6b829e8a5c2548c78
donikamarkowa <donimarkova@gmail.com>
2024-06-04 11:22:17 +0300

    Add new category service with method for fetching all categories

---
+++ src/services/categoryService.tsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_URL = 'https://localhost:7048/api/WorkoutCategory'; 
+
+export interface WorkoutCategoryDto{
+  id: string;
+  name: string
+}
+
+export const fetchCategories = async (authToken: string): Promise<WorkoutCategoryDto[]> =>{
+  const response = await axios.get<WorkoutCategoryDto[]>(`${API_URL}/all`, {
+    headers: {
+      Authorization: `Bearer ${authToken}` 
+    }
+  });
+  return response.data;
+};
+
+
