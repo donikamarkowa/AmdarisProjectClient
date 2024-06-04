534862a447121b9e1ae6a8c2991474e0653058c0
donikamarkowa <donimarkova@gmail.com>
2024-06-04 11:20:57 +0300

    Implement search workouts by criteria endpoint, by category and by trainer

--- src/services/workoutService.tsx
+++ src/services/workoutService.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://localhost:7048/api'; 
+const API_URL = 'https://localhost:7048/api/Workout'; 
 
 export interface WorkoutDto {
     id: string;
@@ -28,7 +28,7 @@ export interface WorkoutDetailsDto {
 }
 
 export const getWorkouts = async (paginationParameters: PaginationParameters,  authToken: string): Promise<WorkoutDto[]> => {
-  const response = await axios.get<WorkoutDto[]>(`${API_URL}/Workout/all`, {
+  const response = await axios.get<WorkoutDto[]>(`${API_URL}/all`, {
     params: paginationParameters,
     headers: {
       Authorization: `Bearer ${authToken}` 
@@ -38,7 +38,7 @@ export const getWorkouts = async (paginationParameters: PaginationParameters,  a
 };
 
 export const getWorkoutDetails = async (id: string, authToken: string): Promise<WorkoutDetailsDto> => {
-    const response = await axios.get<WorkoutDetailsDto>(`${API_URL}/Workout/details?id=${id}`, {
+    const response = await axios.get<WorkoutDetailsDto>(`${API_URL}/details?id=${id}`, {
       params: { id },
       headers: {
         Authorization: `Bearer ${authToken}` 
@@ -46,3 +46,36 @@ export const getWorkoutDetails = async (id: string, authToken: string): Promise<
     });
     return response.data;
 };
+
+export const searchWorkouts = async (criteria: string, paginationParameters: PaginationParameters, authToken: string): Promise<WorkoutDto[]> => {
+  const response = await axios.get(`${API_URL}/search?criteria=${criteria}`, {
+      headers: {
+          Authorization: `Bearer ${authToken}`
+      },
+      params: {
+        params: { criteria, ...paginationParameters },
+      }
+  });
+  return response.data;
+};
+
+export const searchByCategory = async (id: string, authToken: string): Promise<WorkoutDto[]> => {
+  const response = await axios.get<WorkoutDto[]>(`${API_URL}/searchByCategory`, {
+    params: { id: id},
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  });
+  return response.data;
+};
+
+export const searchByTrainer = async (trainerId: string, authToken: string): Promise<WorkoutDto[]> => {
+  const response = await axios.get(`${API_URL}/searchByTrainer`, {
+    params: { id: trainerId},
+      headers: {
+          Authorization: `Bearer ${authToken}`
+      },
+  });
+  return response.data;
+};
+
