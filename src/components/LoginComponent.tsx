8817b03259799192e4f49c91aeb13af246862ae2
donikamarkowa <donimarkova@gmail.com>
2024-06-18 11:24:19 +0300

    Refactor LoginComponent styling and button color

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -37,38 +37,46 @@ const Login: React.FC = () => {
     };
 
     return (
-        <Container maxWidth="sm" sx={{ mt: 5 }}>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Typography variant="h5" gutterBottom>
-              Login
-            </Typography>
-          </Box>
-          <TextField
-            label="Email"
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            margin="normal"
-            fullWidth
-            required
-          />
-          <TextField
-            label="Password"
-            type="password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            margin="normal"
-            fullWidth
-            required
-          />
-          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-            <Button variant="contained" disabled={isLoading} onClick={handleLogin} className="login-buton">
-              {isLoading ? <CircularProgress size={24} color="secondary" /> : 'Login'}
-            </Button>
-          </Box>
-        </Container>
-      );
-    };
+      <Container maxWidth="sm" sx={{ mt: 5 }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Typography variant="h5" gutterBottom className="login-title">
+            Login
+          </Typography>
+        </Box>
+        <TextField
+          label="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          margin="normal"
+          fullWidth
+          required
+          className="login-input"
+        />
+        <TextField
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          margin="normal"
+          fullWidth
+          required
+          className="login-input"
+        />
+        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+          <Button
+            variant="contained"
+            disabled={isLoading}
+            onClick={handleLogin}
+            className="login-button"
+          >
+            {isLoading ? <CircularProgress size={24} color="secondary" /> : 'Login'}
+          </Button>
+        </Box>
+      </Container>
+    );
+  };
+  
 
 export default Login;
 
